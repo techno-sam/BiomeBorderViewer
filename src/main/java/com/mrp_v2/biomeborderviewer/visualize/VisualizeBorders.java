d46eb01fa80069702fe0292b96c6c8088474a0ba
Matthew Partington <matthrewp@gmail.com>
2020-03-23 18:52:22 -0400

    organized imports

--- src/main/java/com/mrp_v2/biomeborderviewer/visualize/VisualizeBorders.java
+++ src/main/java/com/mrp_v2/biomeborderviewer/visualize/VisualizeBorders.java
@@ -3,10 +3,8 @@ package com.mrp_v2.biomeborderviewer.visualize;
 import java.util.ArrayList;
 
 import org.apache.logging.log4j.LogManager;
-import org.lwjgl.opengl.GL11;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import com.mrp_v2.biomeborderviewer.BiomeBorderViewer;
 import com.mrp_v2.biomeborderviewer.config.ConfigOptions;
@@ -18,10 +16,7 @@ import com.mrp_v2.biomeborderviewer.util.Vector3Float;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.Matrix4f;
-import net.minecraft.client.renderer.RenderState;
-import net.minecraft.client.renderer.RenderState.AlphaState;
 import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
