4e1930260f1ca8611299221c152257e53a0a69b4
mrp-v2 <matthrewp@gmail.com>
2020-04-14 11:44:16 -0400

    added a class for storing chunk biome border datas

---
+++ src/main/java/com/mrp_v2/biomeborderviewer/util/ChunkBiomeBorderData.java
@@ -0,0 +1,26 @@
+package com.mrp_v2.biomeborderviewer.util;
+
+import java.util.ArrayList;
+
+public class ChunkBiomeBorderData {
+	private ArrayList<LineData> lines;
+	private ArrayList<CornerData> corners;
+	
+	public ChunkBiomeBorderData(ArrayList<LineData> lines) {
+		this.lines = lines;
+		corners = null;
+	}
+	
+	public ChunkBiomeBorderData(ArrayList<LineData> lines, ArrayList<CornerData> corners) {
+		this.lines = lines;
+		this.corners = corners;
+	}
+	
+	public ArrayList<LineData> getLines(){
+		return lines;
+	}
+	
+	public ArrayList<CornerData> getCorners(){
+		return corners;
+	}
+}
