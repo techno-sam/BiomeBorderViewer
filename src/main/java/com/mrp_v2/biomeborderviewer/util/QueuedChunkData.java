4c173cce05e69c4d716d3de275f67463f280272b
mrp-v2 <matthrewp@gmail.com>
2020-04-14 13:19:27 -0400

    added a class to store data for queued chunks

---
+++ src/main/java/com/mrp_v2/biomeborderviewer/util/QueuedChunkData.java
@@ -0,0 +1,22 @@
+package com.mrp_v2.biomeborderviewer.util;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.world.chunk.IChunk;
+
+public class QueuedChunkData {
+	private final IChunk chunk;
+	private final IWorld world;
+	
+	public QueuedChunkData(IChunk chunk, IWorld world) {
+		this.chunk = chunk;
+		this.world = world;
+	}
+	
+	public IChunk getChunk() {
+		return chunk;
+	}
+	
+	public IWorld getWorld() {
+		return world;
+	}
+}
